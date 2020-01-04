cd95b0ca3fcb1e2aceccc2eb73e3ebaad23a35b4
Vanya Smirnov <Vanya.120101@yandex.ru>
2020-01-04 14:23:05 +0300

    Реализовано чтение существующего пользователя и добавление нового.

--- Fitnes/CodeBlogFitness/CodeBlogFitness.CMD/Program.cs
+++ Fitnes/CodeBlogFitness/CodeBlogFitness.CMD/Program.cs
@@ -12,23 +12,58 @@ namespace CodeBlogFitness.CMD
 
             Console.WriteLine("Введите имя пользователя");
             var name = Console.ReadLine();
+            
+            var userController = new UserController(name);
+            if (userController.IsNewUser)
+            {
+                Console.Write("Введите пол: ");
+                var gender = Console.ReadLine();
+                var birthDate = ParseDateTime();
+                double weight = ParseDouble("вес");
+                double height = ParseDouble("рост");
 
-            Console.WriteLine("Введите пол");
-            var gender = Console.ReadLine();
+                userController.SetNewUserData(gender, birthDate, weight, height);
+            }
+            Console.WriteLine(userController.CurrentUser);
+            Console.ReadLine();
 
-            Console.WriteLine("Введите дату рождения");
-            var birthdate = DateTime.Parse(Console.ReadLine()); // TODO: переписать
+            
+        }
 
+        private static DateTime ParseDateTime()
+        {
+            DateTime birthDate;
+            while (true)
+            {
+                Console.Write("Введите дату рождения (dd.MM.yyyy): ");
+                if (DateTime.TryParse(Console.ReadLine(), out birthDate))
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Неверный формат даты рождения");
+                }
+            }
 
-            Console.WriteLine("Введите вес");
-            var weight = double.Parse(Console.ReadLine());
+            return birthDate;
+        }
 
+        private static double ParseDouble(string name)
+        {
+            while (true)
+            {
+                Console.Write($"Введите {name}: ");
+                if (double.TryParse(Console.ReadLine(), out double value))
+                {
+                    return value;
+                }
+                else
+                {
+                    Console.WriteLine($"Неверный формат {name}");
+                }
+            }
 
-            Console.WriteLine("Введите рост");
-            var height = double.Parse(Console.ReadLine());
-
-            var userController = new UserController(name,gender, birthdate, weight, height);
-            userController.Save();
         }
     }
 }
